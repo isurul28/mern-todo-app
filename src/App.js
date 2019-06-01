9f06c873310d8cdbaba731d0dc79a6f454c90702
Isuru <isuruliyanage1@gmail.com>
2019-06-01 17:11:19 +0530

    hghg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ToDoList from './component/ToDoList';
+import EditList from './component/EditList';
+import CreateList from './component/CreateList';
+
 
 class App extends Component {
   render() {
     return (
-      
-        <div className="container">
-          <h2>MERN-Stack Todo App</h2>
-        </div>
+      <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+            
+          </a>
+          <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Todo</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br/>
+       <Route path="/" exact component={ToDoList}/>
+       <Route path="/edit/:id" exact component={EditList}/>
+       <Route path="/create" exact component={CreateList}/>
+       </div>
+        </Router>
       
     );
   }
---
+++ src/component/EditList.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class EditList extends Component{
+    render(){
+        return(
+            <div>
+                Edit List
+            </div>
+
+        )
+    }
+}
+
+export default EditList;
